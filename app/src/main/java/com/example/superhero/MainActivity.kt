a6fc51cafcaba4e3530308f8177e3fb8fb41aa37
Emmutua <emmamulwa2020@gmail.com>
2023-01-28 18:05:30 +0300

    added appBar

--- app/src/main/java/com/example/superhero/MainActivity.kt
+++ app/src/main/java/com/example/superhero/MainActivity.kt
@@ -5,18 +5,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
+import androidx.compose.material.*
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.superhero.components.HeroCard
 import com.example.superhero.model.HeroData.heroes
 import com.example.superhero.ui.theme.SuperheroesTheme
@@ -42,12 +45,20 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun App() {
     Scaffold(
-        topBar = { }
+        topBar = { AppBar() }
     ) {
         LazyColumn(modifier = Modifier.background(MaterialTheme.colors.background)){
             items(heroes){
 HeroCard(hero = it)
             }
         }
+    }
+}
+
+@Composable
+fun AppBar(modifier: Modifier = Modifier) {
+    Row (modifier = Modifier.background(MaterialTheme.colors.surface), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center){
+       Text(stringResource(R.string.app_name),style = MaterialTheme.typography.h1,
+           fontSize = 40.sp)
     }
 }
--- app/src/main/java/com/example/superhero/components/heroDataSource.kt
+++ app/src/main/java/com/example/superhero/components/heroDataSource.kt
@@ -21,13 +21,13 @@ import com.example.superhero.model.Hero
 
 @Composable
 fun HeroCard(hero: Hero,modifier: Modifier = Modifier) {
-    Card(elevation = 2.dp, modifier = Modifier.padding(8.dp)) {
+    Card(elevation = 2.dp, modifier = Modifier.padding(8.dp).background(MaterialTheme.colors.surface)) {
         Row(
             Modifier
                 .padding(16.dp)
                 .fillMaxWidth()
                 .sizeIn(minHeight = 72.dp)
-                .background(MaterialTheme.colors.surface), verticalAlignment = Alignment.CenterVertically) {
+                , verticalAlignment = Alignment.CenterVertically) {
             Column (modifier = Modifier.weight(1f)){
                 Text(text = stringResource(hero.nameRes), style = MaterialTheme.typography.h3, fontSize = 25.sp)
                 Text(text = stringResource(hero.descriptionRes), style = MaterialTheme.typography.body1)
